0fb3ce0965f0f7f58f48c20a20a9681e60b0e899
Oyewunmi Gbenga <kelvinoye@gmail.com>
2025-03-02 10:32:09 +0100

    Formatted .tf files

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 module "vpc" {
-  source         = "./modules/vpc"
-  vpc_cidr       = var.vpc_cidr
-  public_subnets = var.public_subnets
-  private_subnets= var.private_subnets
-  azs            = var.azs
+  source          = "./modules/vpc"
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  azs             = var.azs
 }
 
 module "bastion" {
@@ -24,23 +24,23 @@ module "alb" {
 }
 
 module "asg" {
-  source                = "./modules/asg"
-  vpc_id                = module.vpc.vpc_id
-  private_subnets       = module.vpc.private_subnets_ids
-  alb_target_group_arn  = module.alb.target_group_arn
-  ami_id                = var.ami_id
-  instance_type         = var.instance_type
-  alb_sg_id             = module.alb.alb_sg_id
+  source               = "./modules/asg"
+  vpc_id               = module.vpc.vpc_id
+  private_subnets      = module.vpc.private_subnets_ids
+  alb_target_group_arn = module.alb.target_group_arn
+  ami_id               = var.ami_id
+  instance_type        = var.instance_type
+  alb_sg_id            = module.alb.alb_sg_id
 }
 
 module "rds" {
-  source            = "./modules/rds"
-  vpc_id            = module.vpc.vpc_id
-  private_subnets   = module.vpc.private_subnets_ids
-  db_username       = var.db_username
-  db_password       = var.db_password
-  db_name           = var.db_name
-  wordpress_sg_id   = module.asg.wordpress_sg_id
+  source          = "./modules/rds"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets_ids
+  db_username     = var.db_username
+  db_password     = var.db_password
+  db_name         = var.db_name
+  wordpress_sg_id = module.asg.wordpress_sg_id
 }
 
 module "efs" {
@@ -51,8 +51,8 @@ module "efs" {
 }
 
 module "route53" {
-  source      = "./modules/route53"
-  domain_name = var.domain_name
-  alb_dns_name= module.alb.alb_dns_name
-  alb_zone_id = module.alb.alb_zone_id
+  source       = "./modules/route53"
+  domain_name  = var.domain_name
+  alb_dns_name = module.alb.alb_dns_name
+  alb_zone_id  = module.alb.alb_zone_id
 }
